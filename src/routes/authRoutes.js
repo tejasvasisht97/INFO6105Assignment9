4ca9666698638bf12d9b3292f08d88d2c986b3a8
tejasvasisht97 <vasisht.t@northeastern.edu>
2023-11-29 11:57:06 -0500

    Controller Update

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Authentication routes (e.g., login)
-router.post('/login', authController.login);
-
-module.exports = router;
--- src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// User routes (e.g., create, edit, delete)
-// ...
-
-module.exports = router;
